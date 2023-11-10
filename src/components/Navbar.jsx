2b15b935285622c92f04b5c2ef2af8789b0c5324
Aryainguz <aryaninguz369@gmail.com>
2023-11-10 20:50:07 +0530

    "made Navbar sticky"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           aria-controls="mobile-menu"
           aria-expanded="false"
-          onClick={() => {toggleMobileMenu()}}
+          onClick={() => toggleMobileMenu()}
         >
           <svg
             className="block h-6 w-6"
