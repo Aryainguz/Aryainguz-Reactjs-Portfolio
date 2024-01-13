c3ab47786048b9dccad9effa0df2392cd9e2e4c3
Aryainguz <aryaninguz369@gmail.com>
2024-01-13 21:58:41 +0530

    added footer component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,7 +34,7 @@ const ExperienceArray = [
 
 const Card = (props) => {
   return(
-<div className="xl:w-1/4 md:w-1/2 p-4">
+<div className="xl:w-1/4 md:w-1/2 p-4 ">
     <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
       <img
         className="h-40 rounded w-full object-cover object-center mb-6"
@@ -58,19 +58,20 @@ const Card = (props) => {
 const Experience = () => {
   return (
     <>
-      <section className="text-gray-400 body-font bg-gray-900 p-6">
+      <section className="text-gray-400 body-font bg-gray-900 px-6">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20">
-            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
-                My Experience
-              </h1>
-              <div className="h-1 w-20 bg-indigo-500 rounded" />
-            </div>
-            <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">
-            </p>
+          <div className="flex flex-wrap w-full">
           </div>
-          <div className="flex flex-wrap -m-4">
+          <div className="lg:text-center">
+        <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase">
+          About Me
+        </h2>
+        <p className="mt-2 text-3xl text-white leading-8 font-extrabold tracking-tight sm:text-4xl">
+          {"{"} About {"}"}
+        </p>
+      </div>
+ 
+          <div className="flex mt-12 flex-wrap -m-4">
 
             {
               ExperienceArray.map((item,index) => (
