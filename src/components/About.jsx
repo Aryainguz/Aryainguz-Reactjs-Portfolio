8bb3b6ab3f58a0ff24b955590845e72d6bc651cd
Aryainguz <aryaninguz369@gmail.com>
2024-01-23 23:40:22 +0530

    added achievements section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,46 +1,29 @@
 import React from 'react'
-
+import {FaLocationArrow,FaCode,FaGithub} from 'react-icons/fa'
+import {SiGmail} from 'react-icons/si'
 
 const About = () => {
   return (
-    <section className="bg-gray-900 text-white w-full py-20">
+    <section className="bg-gray-900 text-white w-full py-20" id='about'>
     <div className="max-w-7xl mx-6 px-4 sm:px-6 sm:mx-auto lg:px-8">
       <div className="lg:text-center">
-        <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase">
+        <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase" data-aos="flip-right"
+          >
           About Me
         </h2>
-        <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">
+        <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl" data-aos="fade-in"
+          >
           {"{"} About {"}"}
         </p>
       </div> 
       <br />
       <div className="mt-10">
         <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-          <div className="flex">
+          <div className="flex" data-aos="zoom-in-right"
+          >
             <div className="flex-shrink-0">
               <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                {/* Heroicon name: globe-alt */}
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 14l9-5-9-5-9 5 9 5z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4.5 9.5l7 3.5m0 0l7-3.5M4.5 14.5l7 3.5m0 0l7-3.5"
-                  />
-                </svg>
+               <FaLocationArrow className='h-6 w-6' />
               </div>
             </div>
             <div className="ml-4">
@@ -55,43 +38,11 @@ const About = () => {
               </dd>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex" data-aos="zoom-in-left"
+          >
             <div className="flex-shrink-0">
               <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                {/* Heroicon name: scale */}
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 6c0-1.1.9-2 2-2h6a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2V6z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 6c0-1.1.9-2 2-2h6a2 2 0 012 2v6a2 2 0 01-2 2h-6a2 2 0 01-2-2V6z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 18c0 1.1.9 2 2 2h6a2 2 0 002-2v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 18c0 1.1.9 2 2 2h6a2 2 0 002-2v-6a2 2 0 00-2-2h-6a2 2 0 00-2 2v6z"
-                  />
-                </svg>
+                <FaCode className='h-6 w-6' />
               </div>
             </div>
             <div className="ml-4">
@@ -101,7 +52,7 @@ const About = () => {
               <dd className="mt-2 text-base text-gray-500">
               {"{"} <br />
               "MERN Stack" : ["Nodejs, "Expressjs", "MongoDB", "Reactjs" ], <br /> 
-              "Nextjs" :    ["SSR", "ISR", "App Router" ], <br /> 
+              "Nextjs" :    ["SSR", "ISR", "App Router", "Prisma", "NextAuth" ], <br /> 
               "DevOps" :    ["Micro Services", "Docker", "Kubernetes" ], <br /> 
               "Django" :    ["Postgres SQL", "MVT model"], <br /> 
               "Languages" : ["Typescript","Javascript", "Python", "C++", "C"], <br />
@@ -109,25 +60,11 @@ const About = () => {
               </dd>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex" data-aos="zoom-in-right"
+          >
             <div className="flex-shrink-0">
               <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                {/* Heroicon name: lightning-bolt */}
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M13 10V3L4 14h7v7l9-11h-7z"
-                  />
-                </svg>
+              <FaGithub className='h-6 w-6' />
               </div>
             </div>
             <div className="ml-4">
@@ -142,25 +79,10 @@ const About = () => {
               </dd>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex" data-aos="zoom-in-left">
             <div className="flex-shrink-0">
               <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                {/* Heroicon name: mail */}
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 6l9 6-9 6M21 6l-9 6 9 6"
-                  />
-                </svg>
+            <SiGmail className='h-6 w-6' />
               </div>
             </div>
             <div className="ml-4">
--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,26 +1,78 @@
 import React from 'react'
-import euphoriacheck from '../assets/euphoriacheck.png'
-
+import bestweb3hack from '../../public/assets/bestweb3hack.jpg'
+import osclead from '../../public/assets/osclead.jpg'
+import acmwinners from '../../public/assets/acmwinners.jpg'
+import ceedfinalist from '../../public/assets/ceedfinalist.jpg'
+import buildspace from '../../public/assets/buildspace.jpg'
+import sih from '../../public/assets/sih.jpg'
+import coffeandcode from '../../public/assets/coffeandcode.jpg'
+import hacktoberfest from '../../public/assets/hacktoberfest.jpg'
+import swocadmin from '../../public/assets/swocadmin.png'
 
 const projectsArray = [
   {
-    img:euphoriacheck,
-    title: 'EuphoriaCheck',
+    img:bestweb3hack,
+    title: 'Winners - Hack4Sustainability, Thapar University',
     description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
     id: 0,
     link: 'https://euphoriacheck.vercel.app/'
   },
-  // {
-  //   img:txwservices,
-  //   title: 'TXW Services',
-  //   description: `A website for The Xiting Way for services website. Built using Nodejs, Expressjs, AOS Animations. With top SEO ranking.`,
-  //   id: 1,
-  //   link: 'https://txwservices.software/'  
-  // },
-  // {
-  //   img:En,
-  //   title:''
-  // }
+  {
+    img:osclead,
+    title: 'Open Source Lead at Open Source Chandigarh',
+    description: `A website for The Xiting Way for services website. Built using Nodejs, Expressjs, AOS Animations. With top SEO ranking.`,
+    id: 1,
+    link: 'https://txwservices.software/'  
+  },
+  {
+    img:acmwinners,
+    title: 'Winners - ACM Present Your Ideas',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 2,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:ceedfinalist,
+    title: 'Ceed Innovest 2.0 Finalist The Xiting Way',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 3,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:buildspace,
+    title: 'buildspace Nights and Weekends S4 - Graduate',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 4,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:sih,
+    title: 'SIH 2023 Top 30 From Chitkara University',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 5,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:coffeandcode,
+    title: 'Special Mention - Coffe and Code Hack 2023, UIET',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 6,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:hacktoberfest,
+    title: 'Hacktoberfest 2023 - Contributor & Maintainer',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 7,
+    link: 'https://euphoriacheck.vercel.app/'
+  },
+  {
+    img:swocadmin,
+    title: 'Project Admin - SWOC 2024 (EuphoriaCheck Top 3)',
+    description: `A Web Application to check the euphoria level of a person using the PERMA model of happiness developed by psychologist Martin Seligman. Built using NodeJS, ExpressJS, EJS, and deployed on Vercel.`,
+    id: 8,
+    link: 'https://euphoriacheck.vercel.app/'
+  }
 ]
 
 
@@ -28,8 +80,8 @@ const LeftProjectCard = ({img,title,description,id,link}) => {
   return (
     <section className="bg-gray-900 p-4">
     <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-      <img className="w-full block" src={img} alt="dashboard image" />
-      <div className="mt-4 md:mt-0">
+      <img className="w-full shadow-lg shadow-black rounded-lg block" src={img} alt="dashboard image" data-aos="zoom-in-right" />
+      <div className="mt-4 md:mt-0" data-aos="zoom-in-left">
         <h2 className="mb-4 text-white font-extrabold text-3xl sm:text-3xl tracking-tight ">{title} </h2>
         <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
          {description}
@@ -61,15 +113,15 @@ const RightProjectCard = ({img,title,description,id,link}) => {
     <section className="bg-gray-900 p-4">
         
     <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-    <img className="w-full block sm:hidden" src={img} alt="dashboard image" />
+    <img className="w-full block shadow-lg shadow-black rounded-lg sm:hidden" src={img} alt="dashboard image" data-aos="zoom-in-left" />
 
-      <div className="mt-4 md:mt-0">
+      <div className="mt-4 md:mt-0" data-aos="zoom-in-right">
         <h2 className="mb-4 text-white font-extrabold text-3xl sm:text-3xl tracking-tight ">{title} </h2>
         <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
          {description}
         </p>
         <a href={link} className="inline-flex items-center text-white bg-indigo-600 hover:bg-primary-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-          View Project
+          Know More
           <svg
             className="ml-2 -mr-1 w-5 h-5"
             fill="currentColor"
@@ -85,7 +137,7 @@ const RightProjectCard = ({img,title,description,id,link}) => {
 
         </a>
       </div>
-      <img className="w-full hidden sm:block" src={img} alt="dashboard image" />
+      <img className="w-full hidden shadow-lg shadow-black rounded-lg sm:block" src={img} alt="dashboard image"  data-aos="zoom-in-left" />
 
     </div>
   </section>
@@ -95,20 +147,27 @@ const RightProjectCard = ({img,title,description,id,link}) => {
 const Projects = () => {
   return (
     <>
-      <section className="bg-gray-900 text-white w-full py-20">
+      <section className="bg-gray-900 text-white w-full py-20" id='achievements'>
         <div className="max-w-7xl mx-6 px-4 sm:px-6 sm:mx-auto lg:px-8">
           <div className="lg:text-center">
-            <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase">
+            <h2 className="text-base text-indigo-400 font-semibold tracking-wide uppercase" data-aos="flip-right">
               My Achievements
             </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl" data-aos="flip-in">
               {"{"} Achievements {"}"}
             </p>
           </div>
         </div>
 
         <LeftProjectCard img={projectsArray[0].img} title={projectsArray[0].title} description={projectsArray[0].description} link={projectsArray[0].link} />
-        <RightProjectCard img={projectsArray[0].img} title={projectsArray[0].title} description={projectsArray[0].description} link={projectsArray[0].link} />
+        <RightProjectCard img={projectsArray[1].img} title={projectsArray[1].title} description={projectsArray[1].description} link={projectsArray[1].link} />
+        <LeftProjectCard img={projectsArray[2].img} title={projectsArray[2].title} description={projectsArray[2].description} link={projectsArray[2].link} />
+        <RightProjectCard img={projectsArray[3].img} title={projectsArray[3].title} description={projectsArray[3].description} link={projectsArray[3].link} />
+        <LeftProjectCard img={projectsArray[4].img} title={projectsArray[4].title} description={projectsArray[4].description} link={projectsArray[4].link} />
+        <RightProjectCard img={projectsArray[5].img} title={projectsArray[5].title} description={projectsArray[5].description} link={projectsArray[5].link} />
+        <LeftProjectCard img={projectsArray[6].img} title={projectsArray[6].title} description={projectsArray[6].description} link={projectsArray[6].link} />
+        <RightProjectCard img={projectsArray[7].img} title={projectsArray[7].title} description={projectsArray[7].description} link={projectsArray[7].link} />
+        <LeftProjectCard img={projectsArray[8].img} title={projectsArray[8].title} description={projectsArray[8].description} link={projectsArray[8].link} />
       </section>
 
     </>
